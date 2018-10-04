2200d00d68e634f09b2fa1e89b17338ec3fa60fb
Christopher Acornley <chrisacornley@hotmail.co.uk>
2018-10-04 16:22:13 +0100

    Added CLion project to repo

---
+++ cmake-build-debug/CMakeFiles/CPPANN.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPANN.dir/source/main.cpp.obj"
+  "CPPANN.pdb"
+  "CPPANN.exe"
+  "CPPANN.exe.manifest"
+  "libCPPANN.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPANN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
